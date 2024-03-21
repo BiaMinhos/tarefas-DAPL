f02ff0379d60a71da816f212eeef6945f4af2d4d
beatriz-minhos <138534948+BiaMinhos@users.noreply.github.com>
2024-03-21 14:03:54 -0300

    tela-de-login-simples

---
+++ TelaDeLoginSimples/TelaDeLoginSimples/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace TelaDeLoginSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bot = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Label();
+            this.Senha = new System.Windows.Forms.Label();
+            this.menu = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bot
+            // 
+            this.bot.BackColor = System.Drawing.Color.HotPink;
+            this.bot.Location = new System.Drawing.Point(192, 205);
+            this.bot.Name = "bot";
+            this.bot.Size = new System.Drawing.Size(76, 23);
+            this.bot.TabIndex = 0;
+            this.bot.Text = "Finalizar";
+            this.bot.UseVisualStyleBackColor = false;
+            this.bot.Click += new System.EventHandler(this.bot_Click);
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Location = new System.Drawing.Point(143, 133);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(29, 13);
+            this.Login.TabIndex = 1;
+            this.Login.Text = "login";
+            // 
+            // Senha
+            // 
+            this.Senha.AutoSize = true;
+            this.Senha.Location = new System.Drawing.Point(142, 173);
+            this.Senha.Name = "Senha";
+            this.Senha.Size = new System.Drawing.Size(36, 13);
+            this.Senha.TabIndex = 2;
+            this.Senha.Text = "senha";
+            // 
+            // menu
+            // 
+            this.menu.AutoSize = true;
+            this.menu.BackColor = System.Drawing.Color.LightPink;
+            this.menu.Location = new System.Drawing.Point(190, 97);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(78, 13);
+            this.menu.TabIndex = 3;
+            this.menu.Text = "Menu de Login";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 4;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(193, 133);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 5;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(193, 170);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(475, 357);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.Senha);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.bot);
+            this.Name = "Form1";
+            this.Text = "Pagina de Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bot;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label Senha;
+        private System.Windows.Forms.Label menu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+    }
+}
+
---
+++ TelaDeLoginSimples/TelaDeLoginSimples/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaDeLoginSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bot_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text;
+            string senha = txtSenha.Text;
+
+            if(login == "admin" && senha == "ds@etefmc")
+            {
+                MessageBox.Show("login concluído divo");
+            }
+            else
+            {
+                MessageBox.Show("login ou senha incorreto");
+            }
+        }
+    }
+}
